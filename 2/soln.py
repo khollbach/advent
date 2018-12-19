c12ec0c0bddadc5ec563dd60248e49d33100e980
Kevan Hollbach <khollbach@gmail.com>
2018-12-19 00:24:48 -0500

    Day 2 part 1

--- 2/soln.py
+++ 2/soln.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 
+from collections import Counter
+
 # Part 1
 
-f = open('input')
+# Read lines and strip whitespace
+ids = list(map(str.strip, open('input').readlines()))
+
+# Does s have any char with exactly freqency freq?
+def has_char_with_freq(freq, s):
+    return any(f == freq for f in Counter(s).values())
+
+# (num words with a 2-frequency-chararacter)
+# * (num words with a 3-frequency-chararacter)
+print(sum(map(lambda x: 1, filter(lambda s: has_char_with_freq(2, s), ids)))
+    * sum(map(lambda x: 1, filter(lambda s: has_char_with_freq(3, s), ids))))
+
+# Part 2
 
 print('TODO')
 assert False
