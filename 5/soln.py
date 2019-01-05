c4ce4c44768fe393341dda7881fe98068bedca1d
Kevan Hollbach <khollbach@gmail.com>
2019-01-04 20:47:40 -0500

    Complete days 3 and 4

---
+++ 5/soln.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import re
+import sys
+
+from collections import namedtuple
+from enum import Enum, auto
+
+# Part 1
+
+# Read in lines, strip whitespace
+lines = []
+for line in open('input').readlines():
+    lines.append(line.strip())
+
+assert False # TODO
