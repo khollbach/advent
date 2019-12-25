b32e63acb2966ed7e7a34519d15ba3edb4e02129
Kevan Hollbach <khollbach@gmail.com>
2019-12-25 17:15:54 -0500

    Begin day 9

---
+++ 2019/python/day9.py
@@ -0,0 +1,12 @@
+from cpu.intcode_computer import IntcodeComputer
+
+import fileinput
+
+def main():
+    program = list(map(int, next(fileinput.input()).split(",")))
+
+    inputs = iter([1])
+    IntcodeComputer(program).run(get_input_fn=lambda: next(inputs))
+
+if __name__ == "__main__":
+    main()
