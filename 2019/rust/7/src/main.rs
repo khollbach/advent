5ba444267ae2ab08d22722384cf6a3a5ee9bd860
Kevan Hollbach <khollbach@gmail.com>
2020-08-17 15:06:44 -0400

    7-2
    Sadly, I had lost all my progress, via `git reset --hard`. I should
    really get out of that habit :/
    
    I re-wrote it all from memory, so we're back to where we were. Lesson
    learned, I hope!

--- 2019/rust/7/src/main.rs
+++ 2019/rust/7/src/main.rs
@@ -1,26 +1,71 @@
 use cpu::misc::read_mem;
 use cpu::CPU;
+use factorial::Factorial;
+use rayon::prelude::*;
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
 
 fn main() {
     let mem = read_mem().unwrap();
 
     println!("{}", day1(&mem));
+    println!("{}", day2(&mem));
+}
+
+/// Amplifiers connected in series.
+fn day1(program: &[i32]) -> i32 {
+    best_signal(program, &[0, 1, 2, 3, 4], thruster_signal_1)
+}
+
+/// Connected in a feedback loop.
+fn day2(program: &[i32]) -> i32 {
+    best_signal(program, &[5, 6, 7, 8, 9], thruster_signal_2)
 }
 
 /// Compute the maximum thruster signal possible, over all permutations of phase settings.
-fn day1(program: &[i32]) -> i32 {
-    permutations(&[0, 1, 2, 3, 4])
-        .into_iter()
-        .map(|p| thruster_signal(&program, &p))
+fn best_signal<F>(program: &[i32], phase_values: &[i32], thruster_signal: F) -> i32
+where
+    F: Fn(&[i32], &[i32]) -> i32 + Sync,
+{
+    permutations(phase_values)
+        .par_iter()
+        .map(|p| thruster_signal(&program, p))
         .max()
         .unwrap()
 }
 
-/// Run the 5 amps in sequence and return the output amplitude for these
-/// phase settings.
-fn thruster_signal(program: &[i32], phase_settings: &[i32]) -> i32 {
+/// This would ideally be lazy (i.e. return an iterator instead of a vector). Not a big deal
+/// though. If rust had a feature like python's generator syntax, this would be trivial...
+fn permutations(values: &[i32]) -> Vec<Vec<i32>> {
+    let n = values.len();
+    if n == 0 {
+        return vec![vec![]];
+    }
+
+    let mut perms = Vec::with_capacity(n.factorial());
+
+    for p in permutations(&values[1..]) {
+        assert!(p.len() == n - 1);
+
+        for i in 0..=p.len() {
+            let mut perm = Vec::with_capacity(n);
+
+            perm.extend(&p[..i]);
+            perm.push(values[0]);
+            perm.extend(&p[i..]);
+
+            perms.push(perm);
+        }
+    }
+
+    perms
+}
+
+/// Run the 5 amps in sequence and return the output amplitude for these phase settings.
+fn thruster_signal_1(program: &[i32], phase_settings: &[i32]) -> i32 {
     let mut signal = 0;
 
     for &phase in phase_settings {
@@ -32,34 +77,67 @@ fn thruster_signal(program: &[i32], phase_settings: &[i32]) -> i32 {
             .run();
 
         assert!(output.borrow().len() == 1);
-        signal = output.borrow_mut()[0];
+        signal = output.borrow()[0];
     }
 
     signal
 }
 
-/// This would ideally be lazy (i.e. return an iterator instead of a vector).
-fn permutations(values: &[i32]) -> Vec<Vec<i32>> {
-    let n = values.len();
-    if n == 0 {
-        return vec![vec![]];
+/// Connect the 5 amps in a feedback loop and return the output amplitude for these phase settings.
+fn thruster_signal_2(program: &[i32], phase_settings: &[i32]) -> i32 {
+    let n = phase_settings.len();
+
+    let chans: Vec<_> = (0..n)
+        .map(|_| channel())
+        .map(|(sx, rx)| (Arc::new(Mutex::new(sx)), Arc::new(Mutex::new(rx))))
+        .collect();
+
+    // Send initial input value of 0, to the first amp.
+    let (sx, _) = &chans[0];
+    sx.lock().unwrap().send(0).unwrap();
+
+    let mut threads = Vec::with_capacity(n);
+    for (i, &phase) in phase_settings.iter().enumerate() {
+        // Receive from "my" channel, send to the "next guy's" channel.
+        let (_, rx) = &chans[i];
+        let (sx, _) = &chans[(i + 1) % n];
+
+        threads.push(run_amp(Vec::from(program), phase, rx.clone(), sx.clone()));
     }
 
-    let mut perms = vec![];
-
-    for p in permutations(&values[1..]) {
-        assert!(p.len() == n - 1);
-
-        for i in 0..=n - 1 {
-            let mut perm = vec![];
-
-            perm.extend(&p[..i]);
-            perm.push(values[0]);
-            perm.extend(&p[i..]);
-
-            perms.push(perm);
-        }
+    for t in threads {
+        t.join().unwrap();
     }
 
-    perms
+    // Get the final output value, from the last amp.
+    let (_, rx) = &chans[0];
+    #[allow(clippy::let_and_return)]
+    let val = rx.lock().unwrap().recv().unwrap();
+    val
+}
+
+fn run_amp(
+    mem: Vec<i32>,
+    phase: i32,
+    input: Arc<Mutex<Receiver<i32>>>,
+    output: Arc<Mutex<Sender<i32>>>,
+) -> JoinHandle<()> {
+    let mut first_input = true;
+
+    thread::spawn(move || {
+        CPU::new(mem)
+            .input_fn(move || {
+                if first_input {
+                    // First input is the phase setting.
+                    first_input = false;
+                    phase
+                } else {
+                    input.lock().unwrap().recv().unwrap()
+                }
+            })
+            .output_fn(move |x| {
+                output.lock().unwrap().send(x).unwrap();
+            })
+            .run();
+    })
 }
