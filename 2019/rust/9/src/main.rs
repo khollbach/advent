39ca64bc21355ad2fdf6ccf08bc760b9ba3f8a57
Kevan Hollbach <khollbach@gmail.com>
2020-08-17 20:22:07 -0400

    9-2

--- 2019/rust/9/src/main.rs
+++ 2019/rust/9/src/main.rs
@@ -4,9 +4,14 @@ use cpu::CPU;
 fn main() {
     let mem = read_mem().unwrap();
 
-    day1(mem);
+    day1(mem.clone());
+    day2(mem);
 }
 
 fn day1(mem: Vec<i64>) {
     CPU::new(mem).input_iter(vec![1].into_iter()).run();
+}
+
+fn day2(mem: Vec<i64>) {
+    CPU::new(mem).input_iter(vec![2].into_iter()).run();
 }
