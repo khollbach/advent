b57fde59a971f8b27a5063556535db9ec30fcbc7
Kevan Hollbach <khollbach@gmail.com>
2020-09-02 01:09:01 -0400

    14-2

--- 2019/rust/14/src/main.rs
+++ 2019/rust/14/src/main.rs
@@ -1,14 +1,65 @@
+#![feature(const_int_pow)]
+
 use graph::{read_graph, Graph};
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::io;
 use std::rc::Rc;
 
 mod graph;
 
+const TRILLION: u64 = 10u64.pow(12);
+
 fn main() {
     let graph = read_graph(io::stdin().lock());
 
-    println!("{}", ore_needed(&graph));
+    println!("{}", ore_needed(&graph, 1));
+
+    println!("{}", fuel_produced(&graph, TRILLION));
+}
+
+/// What is the maximum amount of fuel that can be produced by `ore_available` units of ore.
+///
+/// We use the `ore_needed` function and binary search on all different possible values of fuel,
+/// until we find the maximum value of fuel such that `ore_needed(fuel) <= ore_available`.
+fn fuel_produced(graph: &Graph, ore_available: u64) -> u64 {
+    // I'm not sure what a better choice of upper bound is. Technically, depending on the graph,
+    // the fuel produced could be arbitrarily much, possibly even more than the amount of ore
+    // available.
+    //
+    // Also, if we make `upper` too large, we get overflows in `ore_needed`, so this is actually
+    // quite fragile.
+    let upper = 2u64.pow(40);
+
+    match binary_search(0, upper, ore_available, |fuel| ore_needed(&graph, fuel)) {
+        Ok(fuel) => fuel,
+        Err(fuel) => fuel,
+    }
+}
+
+/// Binary search in the range `lower..upper` to find x such that key(x) == target.
+///
+/// key must be a non-decreasing function on the range lower..upper.
+///
+/// If no such x is found, return an `Err` with the greatest lower bound.
+/// I.e. return some y such that `key(y) < target < key(y+1)`.
+fn binary_search<F>(lower: u64, upper: u64, target: u64, key: F) -> Result<u64, u64>
+where
+    F: Fn(u64) -> u64,
+{
+    use Ordering::*;
+
+    if lower >= upper {
+        return Err(lower - 1);
+    }
+
+    let mid = (lower + upper) / 2;
+
+    match key(mid).cmp(&target) {
+        Less => binary_search(mid + 1, upper, target, key),
+        Greater => binary_search(lower, mid, target, key),
+        Equal => Ok(mid),
+    }
 }
 
 /// Compute a topological ordering of the nodes in `graph.` Doesn't do anything special if there
@@ -54,7 +105,7 @@ fn top_sort(graph: &Graph) -> Vec<Rc<str>> {
 }
 
 /// Compute the indegrees of each node in `graph`.
-fn indegrees(graph: &Graph) -> HashMap<Rc<str>, u32> {
+fn indegrees(graph: &Graph) -> HashMap<Rc<str>, usize> {
     let mut degs: HashMap<_, _> = graph.keys().map(|label| (Rc::clone(label), 0)).collect();
 
     for node in graph.values() {
@@ -78,8 +129,8 @@ fn reverse_mapping(idx_to_label: &[Rc<str>]) -> HashMap<Rc<str>, usize> {
     label_to_idx
 }
 
-/// How much ore to produce one fuel?
-fn ore_needed(graph: &Graph) -> u32 {
+/// How much ore to produce this much fuel?
+fn ore_needed(graph: &Graph, fuel_demand: u64) -> u64 {
     let n = graph.len();
 
     let idx_to_label = top_sort(&graph);
@@ -89,19 +140,19 @@ fn ore_needed(graph: &Graph) -> u32 {
     let ore = label_to_idx["ORE"];
 
     let mut demands = vec![0; n];
-    demands[fuel] = 1;
+    demands[fuel] = fuel_demand;
 
     for i in fuel..ore {
         let chem = &graph[&idx_to_label[i]];
 
         // How many times to perform this reaction.
-        let num_reps = ceiling_divide(demands[i], chem.amount);
+        let num_reps = ceiling_divide(demands[i], chem.amount as u64);
 
         // Propagate demand across edges.
-        for (edge, weight) in &chem.deps {
+        for (edge, &weight) in &chem.deps {
             let j = label_to_idx[edge];
             debug_assert!(j > i);
-            demands[j] += num_reps * weight;
+            demands[j] += num_reps * weight as u64;
         }
     }
 
@@ -113,9 +164,9 @@ fn ore_needed(graph: &Graph) -> u32 {
     demands[ore]
 }
 
-fn ceiling_divide(x: u32, y: u32) -> u32 {
+fn ceiling_divide(x: u64, y: u64) -> u64 {
     assert_ne!(y, 0);
-    (x as f64 / y as f64).ceil() as u32
+    (x as f64 / y as f64).ceil() as u64
 }
 
 #[cfg(test)]
@@ -126,6 +177,52 @@ mod tests {
     #[test]
     fn part1() {
         let graph = read_graph(input!("../tests/input"));
-        assert_eq!(143173, ore_needed(&graph));
+        assert_eq!(143173, ore_needed(&graph, 1));
+        assert_eq!(8845261, fuel_produced(&graph, TRILLION));
+    }
+
+    #[test]
+    fn example1() {
+        let graph = read_graph(input!("../tests/1"));
+        assert_eq!(31, ore_needed(&graph, 1));
+    }
+
+    #[test]
+    fn example2() {
+        let graph = read_graph(input!("../tests/2"));
+        assert_eq!(165, ore_needed(&graph, 1));
+    }
+
+    #[test]
+    fn example3() {
+        let graph = read_graph(input!("../tests/3"));
+        assert_eq!(13312, ore_needed(&graph, 1));
+
+        let fuel = 82892753;
+        assert!(ore_needed(&graph, fuel) <= TRILLION);
+        assert!(ore_needed(&graph, fuel + 1) > TRILLION);
+        assert_eq!(fuel, fuel_produced(&graph, TRILLION));
+    }
+
+    #[test]
+    fn example4() {
+        let graph = read_graph(input!("../tests/4"));
+        assert_eq!(180697, ore_needed(&graph, 1));
+
+        let fuel = 5586022;
+        assert!(ore_needed(&graph, fuel) <= TRILLION);
+        assert!(ore_needed(&graph, fuel + 1) > TRILLION);
+        assert_eq!(fuel, fuel_produced(&graph, TRILLION));
+    }
+
+    #[test]
+    fn example5() {
+        let graph = read_graph(input!("../tests/5"));
+        assert_eq!(2210736, ore_needed(&graph, 1));
+
+        let fuel = 460664;
+        assert!(ore_needed(&graph, fuel) <= TRILLION);
+        assert!(ore_needed(&graph, fuel + 1) > TRILLION);
+        assert_eq!(fuel, fuel_produced(&graph, TRILLION));
     }
 }
